ffe728e62b28abf381c10ea0e3c1894ce72dbe18
Ken Araki <nekikara@gmail.com>
2016-10-06 01:07:03 +0200

    企業ソート表示

--- src/engine.py
+++ src/engine.py
@@ -16,12 +16,22 @@ def user():
     return np.array([1, 2, 2, 1, 1])
 
 def main():
+    matching = []
+    print('')
+    print('######### Start training #########')
     for company in companies():
+        company_id = company[0]
         r, p = pearsonr(user()[1:-1], company[1:-1])
-        print('company id: {}'.format(company[0]))
+        print('company id: {}'.format(company_id))
         print('相関係数 r: {}'.format(r))
         print('有意確率 p: {}'.format(p))
         print('')
+        matching.append([company_id, r])
+    print('######### End training  #########')
+    print('')
+    matching.sort(key=lambda x:x[1], reverse=True)
+    for i, v in enumerate(matching):
+        print('{} 企業: {}, 類似度: {}'.format(i+1, v[0], v[1]))
 
 if __name__ == '__main__':
     main()
